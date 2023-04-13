eb83d448104710145b377c553a06c17714aa7390
MiiKaa3 <m.kasumagic@gmail.com>
2023-04-13 19:56:53 +0200

    updated

--- install_vscode_extensions.sh
+++ install_vscode_extensions.sh
@@ -11,6 +11,7 @@ code --install-extension ms-toolsai.jupyter-renderers
 code --install-extension ms-toolsai.vscode-jupyter-cell-tags
 code --install-extension ms-toolsai.vscode-jupyter-slideshow
 code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.makefile-tools
 code --install-extension ms-vscode.cpptools
 code --install-extension ms-vscode.cpptools-extension-pack
 code --install-extension ms-vscode.cpptools-themes
