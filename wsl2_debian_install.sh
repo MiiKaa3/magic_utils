e911b687bc57e570f85a8f1e4d0c7f00fa632a5e
MiiKaa3 <62884597+MiiKaa3@users.noreply.github.com>
2023-04-13 19:28:43 +0200

    updated

--- wsl2_debian_install.sh
+++ wsl2_debian_install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-sudo apt-get install build-essential python3 python3-pip git vim ranger exa
+sudo apt-get install build-essential wget python3 python3-pip git vim ranger exa
+code .
+./install_vscode_extensions.sh
