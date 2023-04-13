b2d0953222dba1786b523a2c3fe236f2a73f831d
MiiKaa3 <m.kasumagic@gmail.com>
2023-04-13 17:50:07 +0200

    updated

---
+++ install_vscode_extensions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+code --install-extension James-Yu.latex-workshop
+code --install-extension KevinRose.vsc-python-indent
+code --install-extension mathematic.vscode-latex
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
+code --install-extension ms-toolsai.vscode-jupyter-cell-tags
+code --install-extension ms-toolsai.vscode-jupyter-slideshow
+code --install-extension ms-vscode.cmake-tools
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension ms-vscode.cpptools-themes
+code --install-extension ms-vscode.hexeditor
+code --install-extension oguhpereira.spotify-color-theme
+code --install-extension shyykoserhiy.vscode-spotify
+code --install-extension tecosaur.latex-utilities
+code --install-extension twxs.cmake
+code --install-extension valentjn.vscode-ltex
+code --install-extension wholroyd.jinja
--- wsl2_debian_install.sh
+++ wsl2_debian_install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+
 sudo apt-get install build-essential python3 python3-pip git vim ranger exa
