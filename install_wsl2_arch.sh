8c41d325c487aa6bbb3b563ebdf0942e275faecd
MiiKaa3 <m.kasumagic@gmail.com>
2023-04-19 17:06:09 +0200

    updated

---
+++ install_wsl2_arch.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo pacman -S base-devel cmake wget git python python-pip exa ranger vim
+cd /home/michael
+code .
+./install_vscode_extensions.sh
--- install_wsl2_debian.sh
+++ install_wsl2_debian.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo apt-get install build-essential wget python3 python3-pip git vim ranger exa
+cd /home/michael
 code .
 ./install_vscode_extensions.sh
