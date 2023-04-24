7ba6ded5d41fa99c3848c1cf75ce88fd0a9bd607
MiiKaa3 <m.kasumagic@gmail.com>
2023-04-24 19:46:00 +0200

    updated

--- install_wsl2_arch.sh
+++ install_wsl2_arch.sh
@@ -3,3 +3,10 @@
 sudo pacman -S base-devel cmake wget curl git python python-pip exa ranger neovim
 code .
 ./install_vscode_extensions.sh
+cd ~
+mkdir ./.build
+cd ./.build
+git clone https://aur.archlinux.org/yay-bin.git
+cd ./yay-bin
+makepkg -si
+
