3c6c55dae31ae0b89fc28ee49de7d97f3e50157c
MiiKaa3 <62884597+MiiKaa3@users.noreply.github.com>
2023-05-25 19:32:39 +0930

    updated

--- install_wsl2_debian.sh
+++ install_wsl2_debian.sh
@@ -3,3 +3,5 @@
 sudo apt-get install build-essential curl wget python3 python3-pip git neovim ranger exa
 code .
 ./install_vscode_extensions.sh
+cp ./bashrc_user ~/.bashrc
+sudo cp ./bashrc_root /root/.bashrc
